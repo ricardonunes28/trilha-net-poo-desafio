341337d47bf2c89e127b0fd0c742c216e1809f5a
ricardo.nascimento <ricardo.nascimento@globalhitss.com.br>
2024-12-16 22:48:11 -0300

    feat: system modeling for cell phone abstraction

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
+using System.Reflection;
+using System;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Aplicativo {nomeApp} instalado no Iphone");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Snartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "4692123456", modelo: "12354", imei: "856", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Snartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "7777777", modelo: "3256", imei: "857", memoria: 12);
+iphone.Ligar();
+iphone.InstalarAplicativo("Uber");
